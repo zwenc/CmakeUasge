997c7f435307fda38a371a3bac1464a2f09b4325
zwenc <2417935817@qq.com>
2023-11-23 15:08:40 +0800

    [test2] finished

---
+++ test2/app/include/temp.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+
+class myclass {
+
+public:
+    myclass();
+    ~myclass();
+
+    void mytest();
+};
---
+++ test2/library/src/mysum.cpp
@@ -0,0 +1,5 @@
+#include "mysum.h"
+
+int mysum(int a, int b) {
+    return a + b;
+}
